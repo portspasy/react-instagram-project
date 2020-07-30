e090ba733b27538b9e5528c0275c94cc9ed2abbc
Taulant Culi <t.culi91@gmail.com>
2020-07-30 11:46:14 +0200

    creating firebase config file

---
+++ src/firebase/config.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyABfAO1hGY7NBIRKvoFnvUJVGhLreiOSho",
+    authDomain: "firegram-d63df.firebaseapp.com",
+    databaseURL: "https://firegram-d63df.firebaseio.com",
+    projectId: "firegram-d63df",
+    storageBucket: "firegram-d63df.appspot.com",
+    messagingSenderId: "753402046499",
+    appId: "1:753402046499:web:41a22b21e130d2e3b4a996",
+    measurementId: "G-9JK4DSQRL3"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
