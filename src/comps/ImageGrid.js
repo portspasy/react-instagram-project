998cd0ab718a19b3139712199f689dbc5dc8be7e
Taulant Culi <t.culi91@gmail.com>
2020-07-30 17:57:17 +0200

    looping image from collection

--- src/comps/ImageGrid.js
+++ src/comps/ImageGrid.js
@@ -5,7 +5,16 @@ const ImageGrid = () => {
   const { docs } = useFirestore("images");
   console.log(docs);
 
-  return <div className="img-grid"></div>;
+  return (
+    <div className="img-grid">
+      {docs &&
+        docs.map((doc) => (
+          <div className="img-wrap" key={doc.id}>
+            <img src={doc.url} alt="pic" />
+          </div>
+        ))}
+    </div>
+  );
 };
 
 export default ImageGrid;
