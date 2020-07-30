35bb50730ed71c21b9c575618d6a3a4ad193ea66
Taulant Culi <t.culi91@gmail.com>
2020-07-30 17:50:28 +0200

    creating ImageGrid component

---
+++ src/comps/ImageGrid.js
@@ -0,0 +1,11 @@
+import React from "react";
+import useFirestore from "../hooks/useFirestore";
+
+const ImageGrid = () => {
+  const { docs } = useFirestore("images");
+  console.log(docs);
+
+  return <div className="img-grid"></div>;
+};
+
+export default ImageGrid;
